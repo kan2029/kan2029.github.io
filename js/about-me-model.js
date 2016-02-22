86d665a9ec009d2a482d3bd4432048f0c88bedde
Karanbir <karanbir.kajal@commonfloor.com>
2016-02-22 10:16:34 +0530

    separated model js file

---
+++ js/about-me-model.js
@@ -0,0 +1,135 @@
+karan = {};
+karan.model = {
+	basicInfo: {
+		name: 'Karanbir Kajal',
+		traits: ['Web Developer', 'Musician', 'Foodie'],
+		imgSrc: 'img/my-picture.gif',
+		contacts: ['img/linkedin-contact.png', 'img/github-contact.png', 'img/youtube-contact.png', 'img/fb-contact.png', 'img/google-contact.png'],
+	},
+	resume: {
+		name: 'Karanbir Kajal',
+		place: 'Bangalore, Karnataka',
+		email: 'karanbir.kajal@gmail.com',
+		mobile: '+91-9972241602',
+		imageUrl: 'img/resume-my-picture.jpg',
+		socialLinks: [
+			{
+				img: 'img/linkedin.png',
+				link: 'http://linkedin.com/in/karanbir-kajal-8a026688'
+			},
+			{
+				img: 'img/github.png',
+				link: 'http://github.com/kan2029'
+			},
+			{
+				img: 'img/facebook.png',
+				link: 'http://www.facebook.com/karanbirkajla'
+			},
+			{
+				img: 'img/youtube.png',
+				link: 'http://www.youtube.com/user/karanbirkajal'
+			},
+		],
+		skills: [
+			{
+				imgUrl: 'img/cpp.png',
+				label: 'C++'
+			},
+			{
+				imgUrl: 'img/html5.png',
+				label: 'HTML 5'
+			},
+			{
+				imgUrl: 'img/css3.png',
+				label: 'CSS 3'
+			},
+			{
+				imgUrl: 'img/js.png',
+				label: 'Javascript'
+			},
+			{
+				imgUrl: 'img/jquery.png',
+				label: 'JQuery'
+			},
+			{
+				imgUrl: 'img/bootstrap.png',
+				label: 'Bootstrap'
+			},
+			{
+				imgUrl: 'img/react.png',
+				label: 'React'
+			}
+		],
+		//add github, linkedin etc
+		education: [
+			{
+				degree: 'Integrated Dual Degree (B.Tech + M.Tech)',
+				institution: 'IIT Roorkee, Roorkee, Uttarakhand',
+				period: '2009 - 2014',
+				description: 'Electronics and Communication Engineering, specialisation in Wireless Communication with CGPA of 7.8',
+				logo: 'img/iitr.png'
+			},
+			{
+				degree: 'AISSCE',
+				institution: 'Spring Dale Senior School, Amritsar, Punjab',
+				period: '2009',
+				description: 'All India Senior School Certificate Exam (Class 12th) conducted by CBSE with a percentage score of 87.8%',
+				logo: 'img/sdss.png'
+			},
+			{
+				degree: 'AISSE',
+				institution: 'Spring Dale Senior School, Amritsar, Punjab',
+				period: '2007',
+				description: 'All India Secondary School Examination (Class 10th) conducted by CBSE with a percentage score of 89.4%',
+				logo: 'img/sdss.png'
+			}
+		],
+		workExperience:[
+			{
+				position: 'SDE-1 (Front-end)',
+				company: 'Commonfloor.com, Bangalore',
+				logo: 'img/cf.png',
+				period: 'Apr 2015 - Present',
+				description: '- Worked on various projects involving HTML, CSS, JavaScript, JQuery, Bootstrap, React, PHP, MySQL<br>'+ 
+							'- Developed tools for brokers, builders and internal service delivery team to post listing/project details'+
+							'- Worked on advertisements for our homepage<br>'+
+							'- Worked on some features of live-in tour (360 degree views) for mobile devices<br>'+
+							'- Have experience in responsive web design and handling different mobile devices'
+				//add description about react/ customer ads too
+			},
+			{
+				position: 'Manager',
+				company: 'Tata Motors, Pune',
+				logo: 'img/tml.png',
+				period: 'Aug 2014 - Mar 2015',
+				description: "- Was actively involved in dealing with an engineering counterpart and designing product release requirements for 'in-vehicle' apps<br>"+
+				"- Designing these apps was in light with Tata Motor's 'HorizonNext' strategy."
+			}
+		],
+		accomplishments: [
+			{
+				title: 'You Made A Difference',
+				imgSrc: 'img/youMadeADifference.jpg',
+				description: 'Was chosen the best performer for the quarter July - Sept 2015 because I completed all tasks well before their corresponding target dates'					
+			},
+			{
+				title: 'Pat On The Back',
+				imgSrc: 'img/patOnTheBack.jpg',
+				description: 'Was chosen the best fresher of my batch mainly because of my speed of execution and willingness to learn more'
+			},
+		],
+		certifications: [
+			{
+				imgSrc: 'img/frontendNanodegree.jpg'
+			}
+		],
+		references: [
+			{
+
+			},
+			{
+
+			}
+		]
+	}
+}
--- js/about-me.js
+++ js/about-me.js
@@ -1,139 +1,4 @@
-var karan = {
-	model: {
-		basicInfo: {
-			name: 'Karanbir Kajal',
-			traits: ['Web Developer', 'Musician', 'Foodie'],
-			imgSrc: 'img/my-picture.gif',
-			contacts: ['img/linkedin-contact.png', 'img/github-contact.png', 'img/youtube-contact.png', 'img/fb-contact.png', 'img/google-contact.png'],
-		},
-		resume: {
-			name: 'Karanbir Kajal',
-			place: 'Bangalore, Karnataka',
-			email: 'karanbir.kajal@gmail.com',
-			mobile: '+91-9972241602',
-			imageUrl: 'img/resume-my-picture.jpg',
-			socialLinks: [
-				{
-					img: 'img/linkedin.png',
-					link: 'http://linkedin.com/in/karanbir-kajal-8a026688'
-				},
-				{
-					img: 'img/github.png',
-					link: 'http://github.com/kan2029'
-				},
-				{
-					img: 'img/facebook.png',
-					link: 'http://www.facebook.com/karanbirkajla'
-				},
-				{
-					img: 'img/youtube.png',
-					link: 'http://www.youtube.com/user/karanbirkajal'
-				},
-			],
-			skills: [
-				{
-					imgUrl: 'img/cpp.png',
-					label: 'C++'
-				},
-				{
-					imgUrl: 'img/html5.png',
-					label: 'HTML 5'
-				},
-				{
-					imgUrl: 'img/css3.png',
-					label: 'CSS 3'
-				},
-				{
-					imgUrl: 'img/js.png',
-					label: 'Javascript'
-				},
-				{
-					imgUrl: 'img/jquery.png',
-					label: 'JQuery'
-				},
-				{
-					imgUrl: 'img/bootstrap.png',
-					label: 'Bootstrap'
-				},
-				{
-					imgUrl: 'img/react.png',
-					label: 'React'
-				}
-			],
-			//add github, linkedin etc
-			education: [
-				{
-					degree: 'Integrated Dual Degree (B.Tech + M.Tech)',
-					institution: 'IIT Roorkee, Roorkee, Uttarakhand',
-					period: '2009 - 2014',
-					description: 'Electronics and Communication Engineering, specialisation in Wireless Communication with CGPA of 7.8',
-					logo: 'img/iitr.png'
-				},
-				{
-					degree: 'AISSCE',
-					institution: 'Spring Dale Senior School, Amritsar, Punjab',
-					period: '2009',
-					description: 'All India Senior School Certificate Exam (Class 12th) conducted by CBSE with a percentage score of 87.8%',
-					logo: 'img/sdss.png'
-				},
-				{
-					degree: 'AISSE',
-					institution: 'Spring Dale Senior School, Amritsar, Punjab',
-					period: '2007',
-					description: 'All India Secondary School Examination (Class 10th) conducted by CBSE with a percentage score of 89.4%',
-					logo: 'img/sdss.png'
-				}
-			],
-			workExperience:[
-				{
-					position: 'SDE-1 (Front-end)',
-					company: 'Commonfloor.com, Bangalore',
-					logo: 'img/cf.png',
-					period: 'Apr 2015 - Present',
-					description: '- Worked on various projects involving HTML, CSS, JavaScript, JQuery, Bootstrap, React, PHP, MySQL<br>'+ 
-								'- Developed tools for brokers, builders and internal service delivery team to post listing/project details'+
-								'- Worked on advertisements for our homepage<br>'+
-								'- Worked on some features of live-in tour (360 degree views) for mobile devices<br>'+
-								'- Have experience in responsive web design and handling different mobile devices'
-					//add description about react/ customer ads too
-				},
-				{
-					position: 'Manager',
-					company: 'Tata Motors, Pune',
-					logo: 'img/tml.png',
-					period: 'Aug 2014 - Mar 2015',
-					description: "- Was actively involved in dealing with an engineering counterpart and designing product release requirements for 'in-vehicle' apps<br>"+
-					"- Designing these apps was in light with Tata Motor's 'HorizonNext' strategy."
-				}
-			],
-			accomplishments: [
-				{
-					title: 'You Made A Difference',
-					imgSrc: 'img/youMadeADifference.jpg',
-					description: 'Was chosen the best performer for the quarter July - Sept 2015 because I completed all tasks well before their corresponding target dates'					
-				},
-				{
-					title: 'Pat On The Back',
-					imgSrc: 'img/patOnTheBack.jpg',
-					description: 'Was chosen the best fresher of my batch mainly because of my speed of execution and willingness to learn more'
-				},
-			],
-			certifications: [
-				{
-					imgSrc: 'img/frontendNanodegree.jpg'
-				}
-			],
-			references: [
-				{
-
-				},
-				{
-
-				}
-			],
-		},
-	},
-	controller: {
+karan.controller = {
 		init: function(){
 			karan.viewBasicInfo.init();
 		},
@@ -146,8 +11,9 @@ var karan = {
 		initResume: function(){
 			karan.viewResume.initResume()
 		},
-	},
-	viewBasicInfo: {
+	};
+
+	karan.viewBasicInfo = {
 		init: function(){
 			var loaderImg = '<section class="loader-img-wrapper hidden-div"><img class="loader-img" src="img/progress.gif"></section>';
 			$('body').prepend(loaderImg);
@@ -235,8 +101,9 @@ var karan = {
 			}
 		},
 
-	},
-	viewResume: {
+	};
+
+	karan.viewResume = {
 		initResume: function(){
 			if($('#resume').hasClass('hidden')){
 				$('#resume').css('opacity', 0);
@@ -322,8 +189,8 @@ var karan = {
 		    	}, 300);
 			}
 		}
-	},
-};
+	};
+//};
 
 $(document).ready(function(){
 	karan.controller.init();
